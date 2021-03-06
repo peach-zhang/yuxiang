c4c5d00d77e996dc1592051fdde1dfbc22c5760b
zhangtao <tao200967@126.com>
2021-03-06 13:16:31 +0800

    批量上传
    Signed-off-by: zhangtao <tao200967@126.com>

--- src/main/java/com/yuxianglw/controller/UploadController.java
+++ src/main/java/com/yuxianglw/controller/UploadController.java
@@ -43,11 +43,9 @@ public class UploadController {
         res.setContentType("application/octet-stream");
         res.setHeader("Content-Disposition", "attachment; filename=" + fileName);
         byte[] buff = new byte[1024];
-        BufferedInputStream bis = null;
-        OutputStream os = null;
-        try {
-            os = res.getOutputStream();
-            bis = new BufferedInputStream(new FileInputStream("D:\\DataSource\\" + fileName ));
+
+        try (OutputStream os = res.getOutputStream();
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream("D:\\DataSource\\" + fileName ));){
             int i = bis.read(buff);
             while (i != -1) {
                 os.write(buff, 0, buff.length);
@@ -56,14 +54,6 @@ public class UploadController {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (bis != null) {
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
